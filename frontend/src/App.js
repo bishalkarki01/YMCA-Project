1c79bd20b11b86fc3147eddd04fe6e390835c1d9
bishalkarki01 <63300213+bishalkarki01@users.noreply.github.com>
2024-09-16 18:54:35 -0500

    App.js changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 /** @format */
 
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 import HomePage from "./homepage/home";
 import LoginPage from "./login/login";
 import RegistrationPage from "./registration/registration";
+import VisitorPage from "./visitorpage/visitor";
+
+// Where all the webpages go for the router-dom
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<VisitorPage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
