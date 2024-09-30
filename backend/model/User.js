099008215757d2d5db892c8f1c7a28697fcef76a
bishalkarki01 <63300213+bishalkarki01@users.noreply.github.com>
2024-09-30 18:53:02 -0500

    Removed unused dependencies and files

--- backend/model/User.js
+++ backend/model/User.js
@@ -34,6 +34,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema); // Model creation
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
